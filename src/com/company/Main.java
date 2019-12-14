707aa010e25029b037dad5adb381e61aafb843fc
DimetriuS92 <Fakestyle92@gmail.com>
2019-12-14 14:27:01 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Girl girl = new Girl( "Ira" , 22);
+        System.out.println(girl);
+        Girl girl1 = new Girl("Mira" , 24);
+        System.out.println(girl1);
+
+    }
+}
